01a7d9386cd8cce5db78ce14d2869c5792e70b1f
sigtot <sigurdtotland@gmail.com>
2018-02-21 19:01:08 +0100

    CORS stuff?

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-var url = "159.65.58.116";
+var url = "http://159.65.58.116/heis-web/";
 
 $.ajax({
+  xhrFields: {
+    withCredentials: true
+  },
   type: "POST",
+  crossDomain: true,
   url: url
 }).done(function( msg ) {
   console.log(msg);
