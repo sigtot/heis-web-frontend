cf6bb9ff76debf6f2420858f0d3af0b70b709b15
sigtot <sigurdtotland@gmail.com>
2018-02-21 22:38:38 +0100

    Update DOM when model updates

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 var url = "http://159.65.58.116/heis-web/";
 
-$.ajax({
-  xhrFields: {
-    withCredentials: true
-  },
-  type: "POST",
-  crossDomain: true,
-  url: url
-}).done(function( msg ) {
-  console.log(msg);
-});
+var updateView = function() {
+  $.ajax({
+    type: "POST",
+    url: url
+  }).done(function (result) {
+    var modelData = JSON.parse(result);
+    updateDOM(parseInt(modelData["current_floor"]), parseInt(modelData["last_floor"]), parseInt(modelData["door_open"]));
+  });
+};
+
+function updateDOM(currentFloor, lastFloor, doorOpen) {
+  $(".last-floor-number h6").text(lastFloor + 1);
+  if(doorOpen) {
+    $(".doors").each(function(i){
+      if(i === currentFloor) {
+        console.log(i);
+        $(this).addClass("open");
+      } else {
+        $(this).removeClass("open");
+      }
+    });
+
+  } else {
+
+  }
+}
+
+window.setInterval(updateView, 100);
