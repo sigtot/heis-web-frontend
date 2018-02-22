aed4e96a9cc25cbfd13ea18d8e1b9a7d08af5172
sigtot <sigurdtotland@gmail.com>
2018-02-22 22:59:13 +0100

    Fix door not closing

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 var url = "http://159.65.58.116/heis-web/";
+var orderArray = [0,0,0,0]; // 0 is 1st floor
 
 var updateView = function() {
   $.ajax({
     type: "POST",
     url: url
-  }).done(function (result) {
+  }).done(function(result) {
     var modelData = JSON.parse(result);
     updateDOM(parseInt(modelData["current_floor"]), parseInt(modelData["last_floor"]), parseInt(modelData["door_open"]));
   });
 };
+
+function postOrders() {
+  $.ajax({
+    type: "POST",
+    url: url + "orders.php",
+    data: {"orders": orderArray}
+  });
+}
+
+function deleteOrder(floor) {
+  orderArray[floor] = 0;
+}
+
+$(".doors").click(function(i){
+  var floor = 3 - $(".doors").index($(this));
+  orderArray[floor] = 1;
+  postOrders();
+  var element = $(this);
+  element.addClass("pressed");
+  window.setTimeout(function(){
+    element.removeClass("pressed");
+    orderArray[floor] = 0;
+    postOrders();
+  }, 1000);
+});
 
 function updateDOM(currentFloor, lastFloor, doorOpen) {
   $(".last-floor-number h6").text(lastFloor + 1);
